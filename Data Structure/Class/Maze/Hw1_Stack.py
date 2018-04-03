d0d398625ad45350a75e6122130e60e3ba768c06
Younghoon Kim <the_basic_@kookmin.ac.kr>
2018-04-04 03:47:26 +0900

    Add comment

--- Data Structure/Class/Maze/Hw1_Stack.py
+++ Data Structure/Class/Maze/Hw1_Stack.py
@@ -1,4 +1,6 @@
-def is_full():  # Check stack is full?
+# 함수 동작 설명: Hw1.py 파일 주석 참고
+
+def is_full():  # 스택이 가득 찼는지 체크한다.
     global top, stackSize  # 전역 변수로서의 top 사용
     if top == stackSize - 1:
         return 1
@@ -6,7 +8,7 @@ def is_full():  # Check stack is full?
         return 0
 
 
-def is_empty():  # Check stack is empty?
+def is_empty():  # 스택이 비었는지 체크한다.
     global top
     if top == -1:
         return 1
@@ -14,7 +16,7 @@ def is_empty():  # Check stack is empty?
         return 0
 
 
-def push(data):  # Push the data in stack
+def push(data):  # 데이터를 스택에 저장한다.
     global top, stack
     top += 1
     try:
@@ -23,7 +25,7 @@ def push(data):  # Push the data in stack
         print("Err: Maybe Stack is Full")
 
 
-def pop():  # Pop the data
+def pop():  # 마지막으로 저장된 데이터를 스택에서 꺼낸다.
     global top, stack
     try:
         result = stack[top]
@@ -33,7 +35,7 @@ def pop():  # Pop the data
         print("Err : Maybe Stack is Empty")
 
 
-def display_stack():  # Print stack
+def display_stack():  # 스택을 최근 저장된 순서대로 출력한다.
     global top, stack
 
     if is_empty():
@@ -45,7 +47,7 @@ def display_stack():  # Print stack
             print(stack[sp])
             sp -= 1
 
-def display_stack_reverse(row, col):
+def display_stack_reverse(row, col):  # 스택을 아래서 부터 출력한다.
     global top, stack
 
     if is_empty():
