40d6933dd18d7e3c2dbefae1be6b04d3ffa4f4ce
Younghoon Kim <the_basic_@kookmin.ac.kr>
2018-05-13 20:34:34 +0900

    Fix minimum_distance.py

--- Other Solution/minimum_distance.py
+++ Other Solution/minimum_distance.py
@@ -11,19 +11,19 @@ class Distance:
         self.getMin()
 
     def getMin(self):
-        if self.store_list[0] < self.store_list[1]:
-            if self.store_list[0] < self.store_list[2]:  # Min: A
-                print("A %d" % self.store_list[0])
-        elif self.store_list[1] < self.store_list[0]:
-            if self.store_list[1] < self.store_list[2]:  # Min: B
-                print("B %d" % self.store_list[1])
-        elif self.store_list[2] < self.store_list[0]:
-            if self.store_list[2] < self.store_list[1]:  # Min: C
-                print("C %d" % self.store_list[2])
+        if self.store_list[0] < self.store_list[1] and self.store_list[0] < self.store_list[2]:  # Min: A
+            print("A %d" % self.store_list[0])
+        elif self.store_list[1] < self.store_list[0] and self.store_list[1] < self.store_list[2]:  # Min: B
+            print("B %d" % self.store_list[1])
+        elif self.store_list[2] < self.store_list[0] and self.store_list[2] < self.store_list[1]:  # Min: C
+            print("C %d" % self.store_list[2])
 
 
 d1 = Distance()
 d1.getDistance([[1,3], [3,5], [2,4]])
 
 d2= Distance()
-d2.getDistance([[3,7], [1,1], [2,5]])
+d2.getDistance([[3,5], [1,3], [2,4]])
+
+d3 = Distance()
+d3.getDistance([[3,5], [2,4], [1,3]])
