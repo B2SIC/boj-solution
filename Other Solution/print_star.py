3d19942f69809a97dd1f4ffd56fc5a2fb88d7d67
Younghoon Kim <the_basic_@kookmin.ac.kr>
2018-05-17 15:22:18 +0900

    Fix Using while

--- Other Solution/print_star.py
+++ Other Solution/print_star.py
@@ -1,9 +1,13 @@
 num = input("Input your number: ")
 star = "\u2605"
 starnum = 3
+i = 0
 
-for i in range(len(num)):
+while True:
+    if i >= len(num):
+        break
     ch = int(num[i])
     for j in range(starnum * ch):
         print(star, end='')
     print()
+    i += 1
