10774d12334a347ee803661eb5f401f39ae29f44
Younghoon Kim <the_basic_@kookmin.ac.kr>
2018-06-06 23:27:05 +0900

    Add MagicSquare.py

--- Other Solution/MagicSquare.py
+++ Other Solution/MagicSquare.py
@@ -3,7 +3,7 @@ class Square:
         self.square = square
         self.sum = 0
 
-    def isMagicSquare(self):
+    def is_magic_square(self):
         # 기준 값 설정 (첫 번째 행의 합을 구해서 self.sum 에 저장)
         square_sum = 0
         for i in range(1):
@@ -50,7 +50,7 @@ class Square:
         # return False 가 아니라면 행, 열, 대각선의 합이 모두 같다는 의미이므로 True 반환
         return True
 
-    def getSum(self):
+    def get_sum(self):
         return self.sum
 
 # 1 1 1 1
@@ -58,10 +58,10 @@ class Square:
 # 1 1 1 1
 # 1 1 1 1
 sq = Square([[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]])
-print(sq.isMagicSquare())
+print(sq.is_magic_square())
 
 # 1 1 2
 # 1 1 2
 # 1 1 2
 sq2 = Square([[1, 1, 2], [1, 1, 2], [1, 1, 2]])
-print(sq2.isMagicSquare())
+print(sq2.is_magic_square())
